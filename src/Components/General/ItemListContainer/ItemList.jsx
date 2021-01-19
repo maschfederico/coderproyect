c07f6dc0e6034ed46fb54460057121425a219cb9
maschfederico <maschfederico@gmail.com>
2021-01-19 20:12:35 -0300

    Clase 12. Utilizando FireBase

--- src/Components/General/ItemListContainer/ItemList.jsx
+++ src/Components/General/ItemListContainer/ItemList.jsx
@@ -1,23 +1,53 @@
 import {useState, useEffect} from 'react';
 import ProductCard from "../../ProductCard/ProductCard";
-import productos from '../../../DataSource/mockdata/productos';
+import {getFirestore} from '../../../DataSource/firestore';
+import productos from '../../../DataSource/mockdata/productos'
+
 const ItemList = () => {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        getProductos.then(response=> setItems(response));
+        loadProductos();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const getProductos = new Promise((resolve,reject) => {
-        setTimeout(() => {
-            resolve (productos);
-        }, 2000)
-    })
+    const loadProductos = ()=>{
+        const db = getFirestore();
+        db.collection("items").get().then(
+            items =>{
+                let obj = [];
+            items.forEach(doc =>{
+                obj.push({id: doc.id, ...doc.data() })
+                })
+            setItems(obj)    
+            }
+        )
+        
+        
+    };
+
+    const loadProdcutosToFireStore = ()=>{
+        console.log("Carga a Firestore")
+        const db = getFirestore()
+        productos.forEach(
+            producto =>{
+                db.collection("items").add(
+                    producto
+                ).then(()=>{
+                    console.log("Producto cargado:")
+                    console.log(producto.nombre)
+                }).catch(()=>{
+                    console.log(" Error al calrgar Producto:")
+                    console.log(producto.nombre)
+                })
+            }
+        )
+    };
 
    
 
     return ( 
+        <>
         <div className="row" key={1}>
             <div className=" col s12">
                 <span className="title"> Verduras y Frutas</span>
@@ -37,6 +67,13 @@ const ItemList = () => {
             :<p>Cargando Verduras...</p>}
            
         </div>
+        <div className="row" key={2}>
+        <div className=" col s12">
+            Boton de carga de items a fireStore()
+            <button disabled = {true} onClick={loadProdcutosToFireStore}>Cargar data a FireStores</button>
+        </div>
+        </div>
+        </>
      );
 }
  
--- src/Vistas/DetalleProducto.jsx
+++ src/Vistas/DetalleProducto.jsx
@@ -2,20 +2,30 @@ import ProductCard from '../Components/ProductCard/ProductCard'
 import {useParams} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import productos from '../DataSource/mockdata/productos';
+import {getFirestore}  from '../DataSource/firestore'; 
 
 const DetalleProducto = () => {
     const [item, setItem] = useState([]);
     const {productoid} = useParams ();
    
     
-    const ProductoSelected = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          const productoClickeado = productos.find( producto => producto.id === productoid )
-          resolve(productoClickeado);
-        }, 500);
-      });
-    
-    const callProducto = ()=>{ProductoSelected.then((data)=>setItem(data))}
+    const callProducto = ()=>{
+      const db = getFirestore();
+      const producto =  db.collection("items").where('id','==',productoid).get();
+      let obj =[]; // Array de volcado
+      producto.then((docs)=>{
+        docs.forEach(
+          doc => {
+            obj.push(doc.data())
+          }
+        ); 
+        setItem(obj[0]);
+      }
+      ).catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+      
+    }
    
     useEffect(() => {
         callProducto()
